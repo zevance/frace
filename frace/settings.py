55284c23f2be512171a4ea7e0559f790588969fe
zevance <zevance@gmail.com>
2022-03-05 11:49:11 +0200

    static files

--- frace/settings.py
+++ frace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zwdnmu)0!$@08(4ee58_r^*=hvrxlj&*4zu5p2bifi&g%rfu7e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
