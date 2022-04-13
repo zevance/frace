e42c56f435a743dd925f75e09621d11dd83942c3
zevance <42143062+zevance@users.noreply.github.com>
2022-04-13 16:45:41 +0000

    adding models to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from core.models import News, TeamMembers
 
 # Register your models here.
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ['id','name','photo_main','description','phone_number','email_address','position','hire_date']
+    list_display_links = ['id','name','phone_number','email_address']
+    search_fields = ['name','phone_number','email_address']
+    list_per_page = 10
+    
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['id','title','photo_main','description']
+    list_display_links = ['title',]
+    search_fields = ['title',]
+    list_per_page = 10
+    
+admin.site.register(TeamMembers, TeamMemberAdmin)
+admin.site.register(News, NewsAdmin)
