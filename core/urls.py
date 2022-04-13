ccb617fb76f1ff02c895eaceb06168861f8f385e
zevance <42143062+zevance@users.noreply.github.com>
2022-04-13 16:43:43 +0000

    new urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,18 @@
 import imp
 from django.urls import path
-from . import views
+from .views import(
+    AboutUsView,
+    ContactUsView,
+    IndexView,
+    TeamMemberDetailsView,
+    TeamMembersView,
+    MemberDetailsView
+)
 
 urlpatterns = [
-    
+    path('', IndexView.as_view(), name='index'),
+    path('about-us', AboutUsView.as_view(), name='about-us'),
+    path('contact-us', ContactUsView.as_view(), name='contact-us'),
+    path('our-team', TeamMembersView.as_view(), name='our-team'),
+    path('team-member-details/<uuid:pk>', TeamMemberDetailsView.as_view(), name='team-member-details'),
 ]
